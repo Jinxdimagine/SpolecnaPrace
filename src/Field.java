44ad7dc80eb4daf2b88d81338e0dfc6e1a293a6f
Jinxdimagine <martinvu403@gmail.com>
2024-03-15 14:14:45 +0100

    Fixed bug with not showing window.

--- src/Field.java
+++ src/Field.java
@@ -7,13 +7,15 @@ public class Field extends JFrame {
     JButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
     JButton[] functionsButtons = new JButton[9];
     JPanel panel;
+    Game game;
     Field(){
         frame =new JFrame();
         frame.setSize(600,600);
         frame.setResizable(false);
-        frame.setVisible(true);
-
+        addbuttons();
+        addPanel();
         frame.add(panel);
+        frame.setVisible(true);
     }
 
     private void addbuttons(){
