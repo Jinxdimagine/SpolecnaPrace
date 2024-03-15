73a2aeb9b3a1e6edd77e3e08b1b740c28bdd0943
Jinxdimagine <martinvu403@gmail.com>
2024-03-15 14:13:00 +0100

    Created class Game.java.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,10 @@
-public class Game {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Game implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
