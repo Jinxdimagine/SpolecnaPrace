6b9d4e0d052908a15cc63092df56568c719ef196
Bendy545 <130989123+Bendy545@users.noreply.github.com>
2024-03-15 13:04:13 +0100

    Created method that create buttons and added them to array.

--- src/Field.java
+++ src/Field.java
@@ -15,6 +15,26 @@ public class Field extends JFrame {
         frame.add(panel);
     }
 
+    private void addbuttons(){
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        button6 = new JButton();
+        button7 = new JButton();
+        button8 = new JButton();
+        button9 = new JButton();
+        functionsButtons[0] = button1;
+        functionsButtons[1] = button2;
+        functionsButtons[2] = button3;
+        functionsButtons[3] = button4;
+        functionsButtons[4] = button5;
+        functionsButtons[5] = button6;
+        functionsButtons[6] = button7;
+        functionsButtons[7] = button8;
+        functionsButtons[8] = button9;
+    }
 
 
 }
